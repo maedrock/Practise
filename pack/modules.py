478a5b057a181977a96ab56e03cbfb2b1d63722f
Nikolay Simonov <magnasqa@gmail.com>
2024-01-04 10:32:06 +0300

    First publishing of the training

---
+++ pack/modules.py
@@ -0,0 +1,16 @@
+# def print_sum(a, b):
+#     print(a + b)
+
+
+# name = 'Nick'
+
+
+def myfn():
+    pass
+
+
+a = 10
+
+
+# print('modules.py', __name__)
+# print('modules.py', __name__ == '__main__')
